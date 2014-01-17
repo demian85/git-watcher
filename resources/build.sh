bdc39b0d0fd36d00fdac7ce4ec6312cbfdb68968
Demian <demian.rodriguez@avature.net>
2014-01-17 10:25:36 -0300

    Update build script

--- resources/build.sh
+++ resources/build.sh
@@ -2,6 +2,8 @@
 zip -r app.nw ./*
 #copy nw.pak from current build node-webkit
 cp /opt/node-webkit/nw.pak ./nw.pak
+#create directory
+mkdir ../build/linux -p
 #compilation to executable form
 cat /opt/node-webkit/nw ./app.nw > ../build/linux/app && chmod +x ../build/linux/app
 #move nw.pak to build folder
@@ -11,5 +13,3 @@ rm ./app.nw
 #copy useful files
 cp ./run.sh ../build/linux/run.sh
 cp ./config.json ../build/linux/config.json
-#run application
-../build/linux/app
