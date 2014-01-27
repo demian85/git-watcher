7f2330ac904f8bdd772fede24833285b2a01142c
demian <demian85@gmail.com>
2014-01-26 23:37:51 -0300

    run.sh now creates a local symlink to missing library if necessary

--- resources/run.sh
+++ resources/run.sh
@@ -12,4 +12,19 @@ else
 fi
 export LD_LIBRARY_PATH
 
+# Create local symlink to libudev.so.1
+if [ ! -e ./libudev.so.0 ]; then
+    udevDependent=`which udisks 2> /dev/null` # Ubuntu, Mint
+    if [ -z "$udevDependent" ]; then
+        udevDependent=`which systemd 2> /dev/null` # Fedora, SUSE
+    fi
+    if [ -z "$udevDependent" ]; then
+        udevDependent=`which findmnt` # Arch
+    fi
+    udevso=`ldd $udevDependent | grep libudev.so | awk '{print $3;}'`
+    if [ -e "$udevso" ]; then
+       ln -sf "$udevso" ./libudev.so.0
+    fi
+fi
+
 exec -a "$0" "$HERE/app"  "$@"
