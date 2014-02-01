46efbd3d96aca23c69ae8a4bce710eaf2fe1fae3
demian <demian85@gmail.com>
2014-02-01 12:46:03 -0300

    Remove .resources folder, more intuitive structure

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
 #zip all files to nw archive
-zip -r app.nw ./*
+zip -r gitw.nw ./* -x@.gitignore
+
 #copy nw.pak from current build node-webkit
 cp /opt/node-webkit/nw.pak ./nw.pak
+
 #create directory
-mkdir ../build/linux -p
+mkdir ./build/linux -p
+
 #compilation to executable form
-cat /opt/node-webkit/nw ./app.nw > ../build/linux/app && chmod +x ../build/linux/app
+cat /opt/node-webkit/nw ./gitw.nw > ./build/linux/gitw && chmod +x ./build/linux/gitw
+
 #move nw.pak to build folder
-mv ./nw.pak ../build/linux/nw.pak
+mv ./nw.pak ./build/linux/nw.pak
+
 #remove app.nw
 rm ./app.nw
+
 #copy useful files
-cp ./build-package.json ../build/linux/package.json
+cp ./build-package.json ./build/linux/package.json
